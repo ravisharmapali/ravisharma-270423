4bb33750cfa2f3345d552a08edcb8d990590e5a9
ravi <ravisharmapali@gmail.com>
2023-04-29 19:26:32 +0530

    second change

--- src/main/java/com/avisys/cim/CustomerInfoManagementApplication.java
+++ src/main/java/com/avisys/cim/CustomerInfoManagementApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerInfoManagementApplication {
 
 	public static void main(String[] args) {
-		System.out.println("inside main method");
+		System.out.println("main method");
 		SpringApplication.run(CustomerInfoManagementApplication.class, args);
 	}
 
